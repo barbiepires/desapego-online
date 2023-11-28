2359313692002d2d83ee29b0ccfdad25d078edd7
Barbara Pires <venus@pop-os.localdomain>
2023-11-28 13:08:29 -0300

    inicio do projeto

---
+++ javascript/scripts.js
@@ -0,0 +1,56 @@
+var categoria = document.querySelector('#show-or-hide');
+var container = document.querySelector('.container');
+
+categoria.addEventListener('click', function() {
+
+    if(container.style.display === 'block'){
+        container.style.display = 'none';
+    }   else {
+            container.style.display = 'block';
+        }
+            })
+var categoria = document.querySelector('#show-or-hide1');
+var container1 = document.querySelector('.container1');
+            
+categoria.addEventListener('click', function() {
+            
+        if(container1.style.display === 'block'){
+                    container1.style.display = 'none';
+        }   else {
+                container1.style.display = 'block';
+            }
+                })
+var categoria = document.querySelector('#show-or-hide2');
+var container2 = document.querySelector('.container2');
+
+categoria.addEventListener('click', function() {
+            
+        if(container2.style.display === 'block'){
+                    container2.style.display = 'none';
+        }   else {
+                container2.style.display = 'block';
+            }
+                })
+var categoria = document.querySelector('#show-or-hide3');
+var container3 = document.querySelector('.container3');
+
+categoria.addEventListener('click', function() {
+            
+        if(container3.style.display === 'block'){
+                    container3.style.display = 'none';
+        }   else {
+                container3.style.display = 'block';
+            }
+                })      
+var categoria = document.querySelector('#show-or-hide4');
+var container4 = document.querySelector('.container4');
+
+categoria.addEventListener('click', function() {
+            
+        if(container4.style.display === 'block'){
+                    container4.style.display = 'none';
+        }   else {
+                container4.style.display = 'block';
+            }
+                })             
+                        
